74e03dda734c956ad51317c8147d1414440fb633
LordofNotCommitting <shadymcshade247@gmail.com>
2025-10-29 18:37:56 -0400

    first commit

---
+++ src/FreezeItem.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using MGSC;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+
+namespace PerishableNoMore
+{
+
+    [HarmonyPatch(typeof(ItemExpireSystem), nameof(ItemExpireSystem.ProcessExpireItemLogic))]
+    public class FreezeItem
+    {
+        static bool enable_Mod = Plugin.ConfigGeneral.ModData.GetConfigValue<bool>("Enable_Mod", false);
+
+        //passive effect is kept
+        public static bool Prefix(ItemStorage storage, DateTime gameDate)
+        {
+            if (!enable_Mod) {
+                return true;
+            }
+
+            foreach (BasePickupItem basePickupItem in storage.Items)
+            {
+                ExpireComponent expireComponent = basePickupItem.Comp<ExpireComponent>();
+                if (expireComponent != null && expireComponent.IsStarted && expireComponent.ExpireDate < gameDate)
+                {
+
+                    expireComponent.IsStarted = false;
+                }
+            }
+            return false;
+        }
+
+    }
+}
---
+++ src/ModConfigGeneral.cs
@@ -0,0 +1,30 @@
+﻿using MGSC;
+using ModConfigMenu.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PerishableNoMore
+{
+    // Token: 0x02000006 RID: 6
+    public class ModConfigGeneral
+    {
+        // Token: 0x0600001D RID: 29 RVA: 0x00002840 File Offset: 0x00000A40
+        public ModConfigGeneral(string ModName, string ConfigPath)
+        {
+            this.ModName = ModName;
+            this.ModData = new ModConfigData(ConfigPath);
+            this.ModData.AddConfigHeader("STRING:General Settings", "general");
+            this.ModData.AddConfigValue("general", "Enable_Mod", false, "STRING:Enable Mod", "STRING:If you enable this, any item reaching its expiration date will become non-perishable.\n");
+            this.ModData.AddConfigValue("general", "about2", "STRING:<color=#f51b1b>The game must be restarted after setting then saving this config to take effect.</color>\n");
+            this.ModData.RegisterModConfigData(ModName);
+        }
+
+        // Token: 0x04000011 RID: 17
+        private string ModName;
+
+        // Token: 0x04000012 RID: 18
+        public ModConfigData ModData;
+
+    }
+}
